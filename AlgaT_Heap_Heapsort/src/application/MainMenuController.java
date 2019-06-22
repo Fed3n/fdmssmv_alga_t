2659af2f2d2bc627fe70869f9a8201f6d34e370a
Fede <Fede@DESKTOP-FP05J44>
2019-06-22 14:15:26 +0200

    Menu di prova con 2 bottoni

--- AlgaT_Heap_Heapsort/src/application/MainMenuController.java
+++ AlgaT_Heap_Heapsort/src/application/MainMenuController.java
@@ -1,5 +1,26 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainMenuController {
+	
+	@FXML
+	private Button heapButton;
+	
+	@FXML
+	private Button heapsortButton;
+	
+	
+	public void goToHeapLesson(ActionEvent heapPressed) {
+		//TODO Cambia finestra a lezione 1
+		
+	}
+	
+	public void goToHeapsortLesson(ActionEvent heapsortPressed) {
+		//TODO Cambia finestra a lezione 2
+		
+	}
 
 }
