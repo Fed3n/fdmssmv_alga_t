8606ff3e6b59174b8fdfd340c620bd9c9a2c5e4c
Manu <Manu@DESKTOP-J1VE8PM>
2019-06-29 00:15:22 +0200

    Albero con frecce funzionante. Da migliorare data la statisticitá e lunghezza del codice.

--- AlgaT_Heap_Heapsort/src/application/LessonController.java
+++ AlgaT_Heap_Heapsort/src/application/LessonController.java
@@ -22,10 +22,10 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class LessonController { 
-	
+public class LessonController {
+
 	@FXML
-	private Label titleLabel;  
+	private Label titleLabel;
 
     @FXML
     private TextArea lessonText;
@@ -44,12 +44,12 @@ public class LessonController {
 
     @FXML
     private Button questionButton;
-    
+
     private String lessonTitle;
-    
+
     private ArrayList<String> simulLocationList;
     private String fileGetter;
-    
+
     private	Integer textNumber;		//Indica a che pagina della lezione ci si trova
     private final int MAX_LESSON_NUMBER;
 
@@ -70,10 +70,10 @@ public class LessonController {
 		this.textNumber = 1;
 		this.lessonText.setEditable(false);		//Disabilita la scrittura sull'area di testo in cui si legge la lezione
 		this.lessonText.setWrapText(true);
-		
+
 		this.reloadPage();
 	}
-	
+
 	//Importante accertarsi che il numero delle lezioni corrisponda al numero di file text e al numero di righe dentro simulLocation.txt nella cartella corrispondente
 	//Prende in input titolo della lezione, numero delle parti della lezione e nome della cartella in ./ dove si trovano i file
 	//Il numero delle parti della lezione deve essere >=1
@@ -82,44 +82,45 @@ public class LessonController {
 		this.MAX_LESSON_NUMBER = numberOfLessons;
 		this.fileGetter = lessonFileName;
 	}
-	
+
 	public void goBackToMenu(ActionEvent backPressed) throws IOException {
 		Parent mainMenuParent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
     	Scene mainMenuScene = new Scene(mainMenuParent);
-    	
+
     	Stage window = (Stage)((Node)backPressed.getSource()).getScene().getWindow();
-    	
+
     	window.setScene(mainMenuScene);
-    	window.show();		
+    	window.show();
 	}
-	
+
 	public void goToSimulation(ActionEvent simulPressed) throws IOException {
-		Parent heapSimulParent = FXMLLoader.load(getClass().getResource(this.simulLocationList.get(this.textNumber-1)));
+		Parent heapSimulParent = FXMLLoader.load(getClass().getResource("HeapSimulatorSB.fxml"));
+
     	Scene heapSimulScene = new Scene(heapSimulParent);
-    	
+
     	Stage window = (Stage)((Node)simulPressed.getSource()).getScene().getWindow();
-    	
+
     	window.setScene(heapSimulScene);
-    	window.show();		
+    	window.show();
 	}
-	
+
 	//Ricarica la pagina usando i parametri di textNumber
 	public void reloadPage() {
 		//Controllo se sono all'inizio o alla fine della lezione e accendo/spengo i bottoni per avanzare o retrocedere
 		if (this.textNumber == 1)
 			this.prevTextButton.setDisable(true);
 		else this.prevTextButton.setDisable(false);
-		
+
 		if(this.textNumber == MAX_LESSON_NUMBER)
 			this.nextTextButton.setDisable(true);
 		else this.nextTextButton.setDisable(false);
-		
+
 		//Controllo se la parte della lezione ha una simulazione associata o meno, attivo/disattivo pulsante di conseguenza
 		if(this.simulLocationList.get(this.textNumber-1).contentEquals("null")) {
 			this.simulButton.setDisable(true);
 		}
 		else this.simulButton.setDisable(false);
-		
+
 		//Prelevo il testo dalla directory della lezione dal file previsto
 		try {
 			BufferedReader br = Files.newBufferedReader(Paths.get("./"+ this.fileGetter + "/text_" + this.textNumber.toString() + ".txt"), StandardCharsets.UTF_8);
@@ -127,44 +128,44 @@ public class LessonController {
 			String line;
 			while((line = br.readLine()) != null) {
 				text = text + line + "\n";
-			}			
+			}
 			this.lessonText.setText(text);
 		} catch (IOException e) {
 			System.out.println("Couldn't find text_" + this.textNumber.toString() + " in the given directory");
 		}
-		
-		
+
+
 	}
-	
+
 	public void nextText() {
 		this.textNumber++;
 		this.reloadPage();
 	}
-	
+
 	public void prevText() {
 		this.textNumber--;
 		this.reloadPage();
-	}	
-	
-	public void goToQuestions(ActionEvent questionPressed) throws IOException {		
-		
+	}
+
+	public void goToQuestions(ActionEvent questionPressed) throws IOException {
+
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("Question.fxml"));
-		
+
 		//Creo manualmente un controller e lo inizializzo col suo costruttore
 		QuestionController controller = new QuestionController(2, 1, this, false);
 		//Setto manualmente il controller nel loader
 		loader.setController(controller);
-		
+
 		//Creo il parent dal loader con fxml e controller associato *IL FILE FXML NON DEVE AVERE UN CONTROLLER DI DEFAULT*
 		Parent questionParent = (Parent)loader.load();
-		
+
     	Scene questionScene = new Scene(questionParent);
-    	
+
     	Stage window = (Stage)((Node)questionPressed.getSource()).getScene().getWindow();
-    	
+
     	window.setScene(questionScene);
     	window.show();
-    	
+
 	}
-	
+
 }
